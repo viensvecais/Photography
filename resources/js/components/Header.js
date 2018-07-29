c0b5e18021bc9823cf678e128dc9dfe9be3404a2
Mārtiņš Muša <martins@martins-musa.com>
2018-07-29 15:14:54 +0300

    ...

--- resources/js/components/Header.js
+++ resources/js/components/Header.js
@@ -10,16 +10,26 @@ export class Header extends React.Component {
           alt="Hero background"
         />
         <div className="header__content">
-          <h3 className="header__content__brand">
-            Jolanta Līva<br />photography
-          </h3>
-          <h2 className="header__content__title">Pasaule caur manu lēcu</h2>
-          <div className="header__content__horizontal-line" />
-          <h5 className="header__content__subtitle">
+          <div className="header__content__brand">
+            <h5 className="header__content__brand__name">Jolanta Liva</h5>
+            <h6 className="header__content__brand__subt">photography</h6>
+          </div>
+          <h1 className="header__content__title">
+            See the world through my lens
+          </h1>
+          <hr className="header__content__horizontal-line" />
+          <h4 className="header__content__subtitle">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum,
             repellendus.
-          </h5>
+          </h4>
         </div>
+        <a className="header__arrow" href="#">
+          <img
+            className="header__arrow__img"
+            src={require("../../img/arrow.png")}
+            alt="Arrow down"
+          />
+        </a>
       </div>
     );
   }
